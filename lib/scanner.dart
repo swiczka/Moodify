025f4e6cb7d95548895bc635a34f00f4d2366d38
Damian Świeca <damianswieca04@gmail.com>
2025-03-21 23:37:55 +0100

    scanner reads text from taken photos

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:cross_file_image/cross_file_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class Scanner extends StatefulWidget{
   const Scanner({super.key});
@@ -12,11 +16,14 @@ class _ScannerState extends State<Scanner>{
 
   List<CameraDescription> cameras = [];
   CameraController? cameraController;
+  XFile? snapshot;
+  late TextRecognizer textRecognizer;
 
   @override
   void initState() {
     super.initState();
     _setupCameraController();
+    textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
   }
 
   Future<void> _setupCameraController() async{
@@ -41,7 +48,75 @@ class _ScannerState extends State<Scanner>{
     );
   }
 
-  Widget _buildCameraUI(){
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  Future<XFile?> takePicture() async {
+    if (cameraController == null || cameraController?.value.isInitialized == false) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController?.value.isTakingPicture == true) {
+      return null;
+    }
+
+    try {
+      final XFile? file = await cameraController?.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      showInSnackBar(e.description ?? "Camera error");
+      return null;
+    }
+  }
+
+  bool wasPhotoTaken = false;
+
+  Widget buildPreview(){
+    if(!wasPhotoTaken){
+      return CameraPreview(cameraController!);
+    }
+    else{
+      if(snapshot == null){
+        return CameraPreview(cameraController!);
+      }
+      else{
+        return Image(image: XFileImage(snapshot!));
+      }
+    }
+  }
+
+  Future<String?> getTextFromPhoto() async {
+    if(snapshot == null){
+      return "No photo available";
+    }
+    InputImage input = InputImage.fromFile(File(snapshot!.path));
+    RecognizedText text = await textRecognizer.processImage(input);
+    textRecognizer.close();
+    return text.text;
+  }
+
+  Widget buildReadyText() {
+    return FutureBuilder<String?>(
+      future: getTextFromPhoto(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(); // Pokazuje ładowanie
+        } else if (snapshot.hasError) {
+          return Text("Błąd: ${snapshot.error}");
+        } else if (snapshot.data!.isEmpty){
+          return Text("Pusto!");
+        }
+          else{
+          return Text(snapshot.data ?? "Brak tekstu");}
+        }
+    );
+  }
+
+
+  Widget _buildCameraUI() {
     if(cameraController == null || cameraController?.value.isInitialized == false){
       return const Center(child: CircularProgressIndicator());
     }
@@ -52,24 +127,59 @@ class _ScannerState extends State<Scanner>{
               SizedBox(
                   height: MediaQuery.sizeOf(context).height*0.9,
                   width: MediaQuery.sizeOf(context).width,
-                  child: CameraPreview(cameraController!)
+                  child: buildPreview()
               ),
-              Align(
+              if(!wasPhotoTaken) Align(
                 alignment: Alignment.bottomCenter,
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 30.0),
-                  child: FloatingActionButton(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                      elevation: 10,
+                  child: TextButton(
 
-                      onPressed: () {
-                        //TODO what happens when you click a snap button
-                      },
-                    child: Icon(Icons.camera_outlined),
+                    onPressed: () async {
+                      XFile? newSnapshot = await takePicture();
+                      setState(() {
+                        snapshot = newSnapshot;
+                        wasPhotoTaken = true;
+                      });
+                    },
+                    child: Icon(Icons.photo_camera, size: 60, color: Colors.white70),
                   ),
                 ),
               )
-            ],
+              else Stack(
+                children: [Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        wasPhotoTaken = false;
+                        snapshot = null;
+                      });
+                    },
+
+                    child: Icon(Icons.cancel_rounded, color: Colors.white70, size: 40,),
+                    style: ButtonStyle(),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child:
+                      Container(
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            boxShadow: [BoxShadow(blurRadius: 15, spreadRadius: 1, blurStyle: BlurStyle.normal, color: Colors.black45, offset: Offset(10, 10))],
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            color: Colors.white
+                        ),
+                        child: buildReadyText(),
+                      ),
+                    ),
+                  ),
+                ],
+              )],
           )
         )
     );
